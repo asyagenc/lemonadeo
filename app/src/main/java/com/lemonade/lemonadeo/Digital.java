d79c1ff7f959b7f936eeb3c0087bcf5a2edbe6ca
Elif Oskanbaş <115591360+elifoskanbas@users.noreply.github.com>
2024-05-19 22:22:04 +0300

    Update Digital.java

--- app/src/main/java/com/lemonade/lemonadeo/Digital.java
+++ app/src/main/java/com/lemonade/lemonadeo/Digital.java
@@ -87,31 +87,18 @@ public class Digital extends AppCompatActivity {
             }
         });
 
-        final String[] answer = new String[1];
+        final String[] answer = new String[3];
 
         checkk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(minuteRandom==0){
-                    answer[0] =Integer.toString( hourRandom) +" o'clock";}
-                else  if(minuteRandom<15){
-                    answer[0] =Integer.toString( minuteRandom)+" past " +Integer.toString( hourRandom);}
-                else if(minuteRandom==15){
-                    answer[0] =" quarter past "+Integer.toString( hourRandom);}
-                else if(minuteRandom<30){
-                    answer[0] =Integer.toString( minuteRandom)+" past " +Integer.toString( hourRandom);}
-                else if(minuteRandom==30){
-                    answer[0] =" half past "+Integer.toString( hourRandom);}
-                else if(minuteRandom<45){
-                    answer[0] =Integer.toString( 60-minuteRandom)+" to " +Integer.toString( hourRandom +1);}
-                else if(minuteRandom==45){
-                    answer[0] =" quarter to "+Integer.toString( hourRandom +1);}
-                else if(minuteRandom<60){
-                    answer[0] =Integer.toString( 60-minuteRandom)+" to " +Integer.toString( hourRandom +1);}
+                answer[0]=numberToText(hourRandom);
+                answer[1]=numberToText(minuteRandom);
+                answer[2]=answer[0]+" " + answer[1];
 
                 if(!hourInput.getText().toString().isEmpty()) {
-                    if (answer[0].equals(hourInput.getText().toString())) {
+                    if (answer[2].equals(hourInput.getText().toString())) {
                         Toast.makeText(getApplicationContext(), "Congrulations! Correct Answer.", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(getApplicationContext(), "Wrong Answer!", Toast.LENGTH_LONG).show();
@@ -135,5 +122,59 @@ public class Digital extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public static String numberToText(int number) {
+        if (number < 20) {
+            if(number<10){
+                return singleDigitToText(number);}
+            else { return tenToNineteenToText(number);}
+        } else if (number < 30) {
+            return "twenty-" + singleDigitToText(number % 10);
+        } else if (number < 40) {
+            return "thirty-" + singleDigitToText(number % 10);
+        } else if (number < 50) {
+            return "forty-" + singleDigitToText(number % 10);
+        } else if (number < 60) {
+            return "fifty-" + singleDigitToText(number % 10);
+        } else if (number == 60) {
+            return "sixty";
+        } else {
+            return "";
+        }
+    }
+
+
+    public static String singleDigitToText(int digit) {
+        switch (digit) {
+            case 0: return "zero";
+            case 1: return "one";
+            case 2: return "two";
+            case 3: return "three";
+            case 4: return "four";
+            case 5: return "five";
+            case 6: return "six";
+            case 7: return "seven";
+            case 8: return "eight";
+            case 9: return "nine";
+            default: return "";
+        }
+    }
+
+    public static String tenToNineteenToText(int number) {
+        switch (number) {
+            case 10: return "ten";
+            case 11: return "eleven";
+            case 12: return "twelve";
+            case 13: return "thirteen";
+            case 14: return "fourteen";
+            case 15: return "fifteen";
+            case 16: return "sixteen";
+            case 17: return "seventeen";
+            case 18: return "eighteen";
+            case 19: return "nineteen";
+            default: return "";
+        }
     }
 }
